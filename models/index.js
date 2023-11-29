ca9dd0c03446a711cde1d4f46678ab5e6f86ca14
jessgom89 <gomezjessica933@yahoo.com>
2023-11-28 21:07:46 -0800

    aa

--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
 const User = require('./User');
-const Project = require('./Project');
+const Blog= require('./Blog');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+
+User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+Blog.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Blog ,Comment};
